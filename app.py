6651aaaebadd3beb8eff487270de592209c7b5f4
Hamza Aziz <hamzaaziz822@gmail.com>
2024-07-31 21:46:01 +0530

    Commit

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ from parameters import CONFLUENT_KAFKA_IP_ADDRESS, \
     CONFLUENT_KAFKA_PORT, \
     CONFLUENT_KAFKA_TOPIC, \
     DISPLAY_LIST_SIZE, \
-    NETWORK_FAIL_CHECKS, \
     IMAGE_SERVER_ADDRESS, \
     STATUS_THRESHOLD, \
     DGX_IP_ADDRESS, \
+    NETWORK_FAIL_CHECK, \
     PROJECT_SERVICES_PORT
 
 app = Flask(__name__)
@@ -32,14 +32,24 @@ kafka_conf = {
 
 # Create a queue to store messages from kafka
 message_queue = []
-message_to_display = []
+# Create a queue for the messages to display
+messages_to_display = []
+# Keep track of last message update time
 last_update_time = time.time()
+# Flags to keep track of the ERROR and SUCCESS messages sent
+connection_failure_message_sent = False
+project_services_down_message_sent = False
+successful_reconnection_message_sent = not connection_failure_message_sent
+project_restart_message_sent = not project_services_down_message_sent
+
+# initialize a kafka_consumer
+kafka_consumer = None
 
 
 def ping_server(ip: str, port: int):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)
+        sock.settimeout(0.1)
         sock.connect((ip, port))
         return True
     except socket.error:
@@ -51,18 +61,15 @@ def ping_project_services() -> bool:
     for service_port in PROJECT_SERVICES_PORT:
         ping_status.append(ping_server(DGX_IP_ADDRESS, service_port))
 
-    if all(ping_status):
-        return True
-
-    return False
+    return all(ping_status)
 
 
 def post_message_queue_and_get_images(data):
-    global message_to_display
+    global messages_to_display
     try:
         response = requests.post(IMAGE_SERVER_ADDRESS, data=data)
         if response.status_code == 200:
-            message_to_display = response.json()
+            messages_to_display = response.json()
         else:
             print(f"Status Code: {response.status_code}, Message: {response.reason}")
     except Exception as e:
@@ -70,76 +77,117 @@ def post_message_queue_and_get_images(data):
 
 
 def consume_messages():
-    global message_queue, last_update_time, message_to_display
+    global message_queue, last_update_time, messages_to_display, connection_failure_message_sent, project_services_down_message_sent, successful_reconnection_message_sent, project_restart_message_sent
+    global kafka_consumer
 
-    # Create Kafka Consumer
-    consumer = Consumer(kafka_conf)
-    consumer.subscribe([CONFLUENT_KAFKA_TOPIC])
-
-    ping_fail = 0
-    connection_failure_message_sent = False
+    server_ping_fail = 0
+    services_ping_fail = 0
 
     while True:
-        if ping_project_services():
-            ping_fail = 0
+        is_dgx_reachable = ping_server(DGX_IP_ADDRESS, 22)
+        print(f"DGX Reachable: {is_dgx_reachable}")
+        are_services_running = ping_project_services()
+        print(f"Project Running {are_services_running}")
 
-            if connection_failure_message_sent:
-                connection_failure_message_sent = False
+        if is_dgx_reachable:
+            server_ping_fail = 0
+            if not successful_reconnection_message_sent:
                 message_queue = []
-                message = "Facial Recognition System is Getting Started. Please Wait for 2 Minutes."
+                messages_to_display = []
+                message = "🛜 Network Reconnection Successful 🛜"
                 socketio.emit('new_message', {'message': [{'SUCCESS': message}]})
-                # last_update_time = time.time()
-                time.sleep(2 * 60)
-                message = "Facial Recognition System Started. Please Resume Attendance"
-                socketio.emit('new_message', {'message': [{'SUCCESS': message}]})
-                time.sleep(10)
+                successful_reconnection_message_sent = True
+                connection_failure_message_sent = False
+                print(f"\n{message}\n")
 
-            message = consumer.poll(0.1)
-
-            if message is None:
-                print(message)
-
-            if message is not None and message.value() is not None:
-                print(message.value().decode('utf-8'))
-                if message.value().decode('utf-8').startswith("Subscribed topic not available:"):
+            if are_services_running:
+                services_ping_fail = 0
+                if not project_restart_message_sent:
                     message_queue = []
+                    messages_to_display = []
+                    message = "✅ Facial Recognition Attendance System is Getting Started. Please Wait for 2 Minutes ✅"
+                    socketio.emit('new_message', {'message': [{'SUCCESS': message}]})
+                    print(f"\n{message}\n")
+                    time.sleep(2 * 60)
 
-                    while consumer.poll() is None:
-                        continue
+                    message = "Facial Recognition Attendance System Has Been Started. Please Resume Attendance"
+                    socketio.emit('new_message', {'message': [{'SUCCESS': message}]})
+                    print(f"\n{message}\n")
+                    time.sleep(5)
+
+                    project_restart_message_sent = True
+                    project_services_down_message_sent = False
+
+                # Create a Kafka Consumer
+                kafka_consumer = Consumer(kafka_conf)
+                kafka_consumer.subscribe([CONFLUENT_KAFKA_TOPIC])
+
+                message_from_kafka = kafka_consumer.poll(0.2)
+
+                if message_from_kafka is None:
+                    print("No message received.")
                 else:
-                    decoded_message = message.value().decode('utf-8')
-                    if decoded_message.split(": ")[0] == "ERROR":
+                    # Check if it is not an error message sent by Kafka
+                    if message_from_kafka.error():
+                        # Clear existing messages
                         message_queue = []
-                        socketio.emit('new_message', {'message': [{'ERROR': decoded_message.split(": ")[1]}]})
-                    elif decoded_message.split(": ")[0] == "SUCCESS":
-                        message_queue = []
-                        socketio.emit('new_message', {'message': [{'SUCCESS': decoded_message.split(": ")[1]}]})
+                        # while kafka_consumer.poll() is None:
+                        #     continue
                     else:
-                        if decoded_message not in message_queue:
+                        decoded_kafka_message = message_from_kafka.value().decode()
+                        # Check if coming message is an ERROR message or SUCCESS message
+                        if decoded_kafka_message.split(": ")[0] in ["ERROR", "SUCCESS"]:
+                            message_queue = []
+
+                            message_queue.insert(0, decoded_kafka_message)
+                            message_data_to_send = {"messages": message_queue}
+                            json.dumps(message_data_to_send)
+                            post_message_queue_and_get_images(message_data_to_send)
+                            message_queue = []
+                        else:
                             if len(message_queue) >= DISPLAY_LIST_SIZE:
                                 message_queue.pop()
-                            message_queue.insert(0, decoded_message)
-                            print(message_queue)
-                            data = {"messages": message_queue}
-                            data = json.dumps(data)
-                            post_message_queue_and_get_images(data)
+                            message_queue.insert(0, decoded_kafka_message)
+                            message_data_to_send = {"messages": message_queue}
+                            json.dumps(message_data_to_send)
+                            post_message_queue_and_get_images(message_data_to_send)
                             last_update_time = time.time()
+            else:
+                services_ping_fail += 1
+
+                if services_ping_fail >= NETWORK_FAIL_CHECK and not project_services_down_message_sent:
+                    message_queue = []
+                    messages_to_display = []
+                    message = "⚠️ Facial Recognition System Temporarily Down for Maintenance or Due to an Unexpected Issue ⚠️"
+                    socketio.emit('new_message', {'message': [{'ERROR': message}]})
+                    project_services_down_message_sent = True
+                    project_restart_message_sent = False
+                    print(f"\n{message}\n")
+
+                    if kafka_consumer is not None:
+                        kafka_consumer = None
 
         else:
-            ping_fail += 1
-            time.sleep(1)
-            if ping_fail >= NETWORK_FAIL_CHECKS and not connection_failure_message_sent:
-                message_queue.clear()
-                message = "⚠️ Facial Recognition System Temporarily Down for Maintenance or Due to an Unexpected Issue ⚠️"
+            server_ping_fail += 1
+
+            if server_ping_fail >= NETWORK_FAIL_CHECK and not connection_failure_message_sent:
+                message_queue = []
+                messages_to_display = []
+                message = "⚠️ Network Issue: Unable to Connect to the Server ⚠️"
                 socketio.emit('new_message', {'message': [{'ERROR': message}]})
                 connection_failure_message_sent = True
-                last_update_time = time.time()
+                successful_reconnection_message_sent = False
+                project_services_down_message_sent = False
+                print(f"\n{message}\n")
+
+                if kafka_consumer is not None:
+                    kafka_consumer = None
 
         if time.time() - last_update_time >= STATUS_THRESHOLD:
             message_queue = []
-            message_to_display = []
+            messages_to_display = []
+            socketio.emit('new_message', {'message': messages_to_display})
             last_update_time = time.time()
-            socketio.emit('new_message', {'message': message_to_display})
 
 
 kafka_consumer_thread = threading.Thread(target=consume_messages)
@@ -149,20 +197,20 @@ kafka_consumer_thread.start()
 
 @socketio.on('connect')
 def handle_connect():
-    global message_to_display
-    emit('new_message', {'message': message_to_display})
+    global messages_to_display
+    emit('new_message', {'message': messages_to_display})
 
 
 @socketio.on('check_updates')
 def handle_check_updates():
-    global message_to_display
-    emit('new_message', {'message': message_to_display})
+    global messages_to_display
+    emit('new_message', {'message': messages_to_display})
 
 
 @app.route('/')
 def index():
-    global message_to_display
-    return render_template('index.html', message=message_to_display)
+    global messages_to_display
+    return render_template('index.html', message=messages_to_display)
 
 
 if __name__ == "__main__":
--- parameters.py
+++ parameters.py
@@ -8,7 +8,8 @@ PROJECT_SERVICES_PORT = [
     6381,       # Redis
     20001,      # Database-Controller
     20012,      # Kafka-Message-Controller
-    20013       # Display-Image-Server
+    20013,      # Display-Image-Server
+    6386        # Face Liveness Detection
 ]
 
 # Kafka IP and Port
@@ -21,11 +22,10 @@ CONFLUENT_KAFKA_TOPIC = 'feedback_messages_topic_for_tapo-cam-1'
 # Number of names to be displayed at once
 DISPLAY_LIST_SIZE = 4
 
-# Number of times to check for connection failure
-NETWORK_FAIL_CHECKS = 5
-
 # Message Retention time on Screen
 STATUS_THRESHOLD = 15
 
 # Image Server Address
 IMAGE_SERVER_ADDRESS = "http://192.168.12.1:20013/post_images"
+
+NETWORK_FAIL_CHECK = 5
