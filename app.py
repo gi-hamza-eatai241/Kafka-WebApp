c3154ac4225bd040ad34ff9d87c9c55a3a373773
Hamza Aziz <hamzaaziz822@gmail.com>
2024-08-03 14:04:39 +0530

    Added log files

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import requests
 from confluent_kafka import Consumer
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from custom_logging import feedback_client_logger
 from parameters import CONFLUENT_KAFKA_IP_ADDRESS, \
     CONFLUENT_KAFKA_PORT, \
     CONFLUENT_KAFKA_TOPIC, \
@@ -44,6 +45,7 @@ project_restart_message_sent = not project_services_down_message_sent
 
 # initialize a kafka_consumer
 kafka_consumer = None
+# print(f"\nINITIAL STAGE OF KAFKA CONSUMER: {kafka_consumer}\n")
 
 
 def ping_server(ip: str, port: int):
@@ -70,6 +72,7 @@ def post_message_queue_and_get_images(data):
         response = requests.post(IMAGE_SERVER_ADDRESS, data=data)
         if response.status_code == 200:
             messages_to_display = response.json()
+            # print(f"\n MESSAGES TO DISPLAY: {messages_to_display}\n")
         else:
             print(f"Status Code: {response.status_code}, Message: {response.reason}")
     except Exception as e:
@@ -85,9 +88,9 @@ def consume_messages():
 
     while True:
         is_dgx_reachable = ping_server(DGX_IP_ADDRESS, 22)
-        print(f"DGX Reachable: {is_dgx_reachable}")
+        # print(f"DGX Reachable: {is_dgx_reachable}")
         are_services_running = ping_project_services()
-        print(f"Project Running {are_services_running}")
+        # print(f"Project Running: {are_services_running}")
 
         if is_dgx_reachable:
             server_ping_fail = 0
@@ -98,7 +101,8 @@ def consume_messages():
                 socketio.emit('new_message', {'message': [{'SUCCESS': message}]})
                 successful_reconnection_message_sent = True
                 connection_failure_message_sent = False
-                print(f"\n{message}\n")
+                feedback_client_logger.info("Network Reconnection Successful")
+                # print(f"\n{message}\n")
 
             if are_services_running:
                 services_ping_fail = 0
@@ -107,65 +111,87 @@ def consume_messages():
                     messages_to_display = []
                     message = "✅ Facial Recognition Attendance System is Getting Started. Please Wait for 2 Minutes ✅"
                     socketio.emit('new_message', {'message': [{'SUCCESS': message}]})
-                    print(f"\n{message}\n")
+                    feedback_client_logger.info("Facial Recognition Attendance System is Getting Started. Please Wait for 2 Minutes")
+                    # print(f"\n{message}\n")
                     time.sleep(2 * 60)
 
                     message = "Facial Recognition Attendance System Has Been Started. Please Resume Attendance"
                     socketio.emit('new_message', {'message': [{'SUCCESS': message}]})
-                    print(f"\n{message}\n")
-                    time.sleep(5)
+                    feedback_client_logger.info(message)
+                    # print(f"\n{message}\n")
 
                     project_restart_message_sent = True
                     project_services_down_message_sent = False
 
-                # Create a Kafka Consumer
-                kafka_consumer = Consumer(kafka_conf)
-                kafka_consumer.subscribe([CONFLUENT_KAFKA_TOPIC])
+                    time.sleep(5)
+
+                if kafka_consumer is None:
+                    # Create a Kafka Consumer
+                    kafka_consumer = Consumer(kafka_conf)
+                    kafka_consumer.subscribe([CONFLUENT_KAFKA_TOPIC])
+                    feedback_client_logger.info("Created a fresh kafka consumer")
+
+                # print(f"\nKAFKA CONSUMER AFTER SUCCESSFUL START OF THE PROJECT: {kafka_consumer}\n")
 
                 message_from_kafka = kafka_consumer.poll(0.2)
 
                 if message_from_kafka is None:
-                    print("No message received.")
+                    pass
+                    # print("No message received.")
                 else:
                     # Check if it is not an error message sent by Kafka
+                    # print(f"\nRAW MESSAGE FROM KAFKA: {message_from_kafka}\n")
                     if message_from_kafka.error():
                         # Clear existing messages
                         message_queue = []
+                        feedback_client_logger.error(f"Received an error message from kafka: {message_from_kafka.error()}")
                         # while kafka_consumer.poll() is None:
                         #     continue
                     else:
                         decoded_kafka_message = message_from_kafka.value().decode()
+                        # print(f"\nMESSAGE RECEIVED FROM KAFKA: {decoded_kafka_message}\n")
                         # Check if coming message is an ERROR message or SUCCESS message
                         if decoded_kafka_message.split(": ")[0] in ["ERROR", "SUCCESS"]:
                             message_queue = []
-
+                            # print(message_queue)
                             message_queue.insert(0, decoded_kafka_message)
+                            # print(message_queue)
                             message_data_to_send = {"messages": message_queue}
-                            json.dumps(message_data_to_send)
-                            post_message_queue_and_get_images(message_data_to_send)
+                            data = json.dumps(message_data_to_send)
+                            post_message_queue_and_get_images(data)
                             message_queue = []
+                            # print(message_queue)
                         else:
-                            if len(message_queue) >= DISPLAY_LIST_SIZE:
-                                message_queue.pop()
-                            message_queue.insert(0, decoded_kafka_message)
-                            message_data_to_send = {"messages": message_queue}
-                            json.dumps(message_data_to_send)
-                            post_message_queue_and_get_images(message_data_to_send)
-                            last_update_time = time.time()
+                            if decoded_kafka_message not in message_queue:
+                                if len(message_queue) >= DISPLAY_LIST_SIZE:
+                                    message_queue.pop()
+                                # print(message_queue)
+                                message_queue.insert(0, decoded_kafka_message)
+                                # print(message_queue)
+                                message_data_to_send = {"messages": message_queue}
+                                data = json.dumps(message_data_to_send)
+                                post_message_queue_and_get_images(data)
+                                last_update_time = time.time()
             else:
                 services_ping_fail += 1
 
-                if services_ping_fail >= 3 and not project_services_down_message_sent:
+                if services_ping_fail >= NETWORK_FAIL_CHECK and not project_services_down_message_sent:
                     message_queue = []
                     messages_to_display = []
                     message = "⚠️ Facial Recognition System Temporarily Down for Maintenance or Due to an Unexpected Issue ⚠️"
                     socketio.emit('new_message', {'message': [{'ERROR': message}]})
                     project_services_down_message_sent = True
                     project_restart_message_sent = False
-                    print(f"\n{message}\n")
+                    feedback_client_logger.error("Facial Recognition System Temporarily Down for Maintenance or Due to an Unexpected Issue")
+                    # print(f"\n{message}\n")
 
                     if kafka_consumer is not None:
                         kafka_consumer = None
+                        feedback_client_logger.warn("Deleted Kafka Consumer as the facial recognition system shutdown.")
+
+                    # print(f"\nKAFKA CONSUMER STATUS IF PROJECT DOWN: {kafka_consumer}\n")
+
+                    services_ping_fail = 0
 
         else:
             server_ping_fail += 1
@@ -178,13 +204,20 @@ def consume_messages():
                 connection_failure_message_sent = True
                 successful_reconnection_message_sent = False
                 project_services_down_message_sent = False
-                print(f"\n{message}\n")
+                feedback_client_logger.error("Network Issue: Unable to Connect to the Server")
+                # print(f"\n{message}\n")
 
                 if kafka_consumer is not None:
                     kafka_consumer = None
+                    feedback_client_logger.warn("Deleted Kafka Consumer as the server is not reachable.")
+
+                # print(f"\nKAFKA CONSUMER STATUS IF SERVER NOT CONNECTED: {kafka_consumer}\n")
+
+                server_ping_fail = 0
 
         if time.time() - last_update_time >= STATUS_THRESHOLD:
             message_queue = []
+            # print(message_queue)
             messages_to_display = []
             socketio.emit('new_message', {'message': messages_to_display})
             last_update_time = time.time()
--- parameters.py
+++ parameters.py
@@ -2,7 +2,7 @@
 DGX_IP_ADDRESS = "192.168.12.1"
 
 PROJECT_SERVICES_PORT = [
-    6385, 19000, 190001,    # InsightFace
+    6385, 19000, 19001,    # InsightFace
     19530,                  # Milvus Vector Database
     9092,                   # Kafka Broker
     6381,                   # Redis Database
@@ -17,7 +17,7 @@ CONFLUENT_KAFKA_IP_ADDRESS = DGX_IP_ADDRESS
 CONFLUENT_KAFKA_PORT = 9092
 
 # Kafka topic for messages
-CONFLUENT_KAFKA_TOPIC = 'feedback_messages_topic_for_tapo-cam-1'
+CONFLUENT_KAFKA_TOPIC = 'feedback_messages_topic_for_library-gate'
 
 # Number of names to be displayed at once
 DISPLAY_LIST_SIZE = 4
@@ -28,4 +28,8 @@ STATUS_THRESHOLD = 15
 # Image Server Address
 IMAGE_SERVER_ADDRESS = f"http://{DGX_IP_ADDRESS}:20013/post_images"
 
+# Number of times to check for network failure
 NETWORK_FAIL_CHECK = 5
+
+# Logs directory path
+LOGS_FOLDER = "logs"
