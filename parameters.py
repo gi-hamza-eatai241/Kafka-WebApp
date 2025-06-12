a2f5e1115e158cec45bcc6776a1cc7fd208a8f91
hamzaziizzz <hamzaaziz822@gmail.com>
2025-06-12 08:54:58 +0530

    Commit

--- parameters.py
+++ parameters.py
@@ -17,8 +17,8 @@ CONFLUENT_KAFKA_IP_ADDRESS = DGX_IP_ADDRESS
 CONFLUENT_KAFKA_PORT = 9092
 
 # Kafka topic for messages
-CONFLUENT_KAFKA_TOPIC = "maingate_exit"
-CONFLUENT_KAFKA_PARTITION = 4
+CONFLUENT_KAFKA_TOPIC = "maingate_entry"
+CONFLUENT_KAFKA_PARTITION = 6
 
 # Number of names to be displayed at once
 DISPLAY_LIST_SIZE = 6
